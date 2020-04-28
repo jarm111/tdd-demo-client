9aa437a692829fabd659f1e1013f64a09da31c9b
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-04-28 12:30:40 +0300

    Add fields to EventForm

--- src/components/EventForm.test.tsx
+++ src/components/EventForm.test.tsx
@@ -3,19 +3,35 @@ import { render, fireEvent, act } from '@testing-library/react'
 import EventForm from './EventForm'
 
 describe('<EventForm/>', () => {
-  it('finds input and calls submit', async () => {
+  it('fills fields and calls submit with values', async () => {
+    const [title, date, description] = [
+      'My new event',
+      '2020-04-28',
+      'Everyone is welcome to my new event!',
+    ]
     const onSubmit = jest.fn()
     const { getByText, getByLabelText } = render(
       <EventForm onSubmit={onSubmit} />
     )
 
     fireEvent.input(getByLabelText('title-input'), {
-      target: { value: 'My new event' },
+      target: { value: title },
+    })
+    fireEvent.input(getByLabelText('date-input'), {
+      target: { value: date },
+    })
+    fireEvent.input(getByLabelText('description-input'), {
+      target: { value: description },
     })
     await act(async () => {
       fireEvent.click(getByText('Submit'))
     })
 
     expect(onSubmit).toHaveBeenCalledTimes(1)
+    expect(onSubmit).toHaveBeenCalledWith({
+      title,
+      date,
+      description,
+    })
   })
 })
--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -7,14 +7,19 @@ type Props = {
 
 const EventForm = ({ onSubmit }: Props) => {
   const { register, handleSubmit, watch, errors } = useForm()
-  const mySubmit = handleSubmit(() => {
-    onSubmit()
+  const mySubmit = handleSubmit((data) => {
+    console.log('form data', data)
+    onSubmit(data)
   })
 
   return (
     <form onSubmit={mySubmit}>
       <label>Title</label>
       <input name="title" ref={register} aria-label="title-input" />
+      <label>Date</label>
+      <input name="date" ref={register} aria-label="date-input" />
+      <label>Description</label>
+      <input name="description" ref={register} aria-label="description-input" />
       <input type="submit" value="Submit" />
     </form>
   )
