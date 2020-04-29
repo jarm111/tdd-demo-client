aa76acfa0321058895df3a56a9f5401b91c1f3c5
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-04-29 12:54:29 +0300

    Add EventForm validation

--- src/components/EventForm.test.tsx
+++ src/components/EventForm.test.tsx
@@ -34,4 +34,95 @@ describe('<EventForm/>', () => {
       description,
     })
   })
+
+  it('handles title validation errors', async () => {
+    const onSubmit = jest.fn()
+    const { getByText, getByLabelText } = render(
+      <EventForm onSubmit={onSubmit} />
+    )
+
+    fireEvent.input(getByLabelText('title-input'), {
+      target: { value: '' },
+    })
+
+    await act(async () => {
+      fireEvent.blur(getByLabelText('title-input'))
+    })
+
+    getByText('Title is required', { exact: false })
+
+    await act(async () => {
+      fireEvent.input(getByLabelText('title-input'), {
+        target: { value: 'a' },
+      })
+    })
+
+    getByText('Title minimum length', { exact: false })
+
+    await act(async () => {
+      fireEvent.input(getByLabelText('title-input'), {
+        target: { value: 'a'.repeat(101) },
+      })
+    })
+
+    getByText('Title maximum length', { exact: false })
+  })
+
+  it('handles date validation errors', async () => {
+    const onSubmit = jest.fn()
+    const { getByText, getByLabelText } = render(
+      <EventForm onSubmit={onSubmit} />
+    )
+
+    fireEvent.input(getByLabelText('date-input'), {
+      target: { value: '' },
+    })
+
+    await act(async () => {
+      fireEvent.blur(getByLabelText('date-input'))
+    })
+
+    getByText('Date is required', { exact: false })
+
+    await act(async () => {
+      fireEvent.input(getByLabelText('date-input'), {
+        target: { value: '2020-05-1' },
+      })
+    })
+
+    getByText('Date must be in format', { exact: false })
+  })
+
+  it('handles description validation errors', async () => {
+    const onSubmit = jest.fn()
+    const { getByText, getByLabelText } = render(
+      <EventForm onSubmit={onSubmit} />
+    )
+
+    fireEvent.input(getByLabelText('description-input'), {
+      target: { value: '' },
+    })
+
+    await act(async () => {
+      fireEvent.blur(getByLabelText('description-input'))
+    })
+
+    getByText('Description is required', { exact: false })
+
+    await act(async () => {
+      fireEvent.input(getByLabelText('description-input'), {
+        target: { value: 'a' },
+      })
+    })
+
+    getByText('Description minimum length', { exact: false })
+
+    await act(async () => {
+      fireEvent.input(getByLabelText('description-input'), {
+        target: { value: 'a'.repeat(501) },
+      })
+    })
+
+    getByText('Description maximum length', { exact: false })
+  })
 })
--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -6,20 +6,61 @@ type Props = {
 }
 
 const EventForm = ({ onSubmit }: Props) => {
-  const { register, handleSubmit, watch, errors } = useForm()
+  const { register, handleSubmit, errors } = useForm({ mode: 'onBlur' })
   const mySubmit = handleSubmit((data) => {
-    console.log('form data', data)
     onSubmit(data)
   })
 
   return (
     <form onSubmit={mySubmit}>
       <label>Title</label>
-      <input name="title" ref={register} aria-label="title-input" />
+      <input
+        name="title"
+        ref={register({
+          required: { value: true, message: 'Title is required' },
+          minLength: {
+            value: 3,
+            message: 'Title minimum length is 3 characters',
+          },
+          maxLength: {
+            value: 100,
+            message: 'Title maximum length is 100 characters',
+          },
+        })}
+        aria-label="title-input"
+        defaultValue=""
+      />
+      {errors.title && errors.title.message}
       <label>Date</label>
-      <input name="date" ref={register} aria-label="date-input" />
+      <input
+        name="date"
+        ref={register({
+          required: { value: true, message: 'Date is required' },
+          pattern: {
+            value: /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/,
+            message: 'Date must be in format: yyyy-mm-dd',
+          },
+        })}
+        aria-label="date-input"
+      />
+      {errors.date && errors.date.message}
       <label>Description</label>
-      <input name="description" ref={register} aria-label="description-input" />
+      <input
+        name="description"
+        ref={register({
+          required: { value: true, message: 'Description is required' },
+          minLength: {
+            value: 10,
+            message: 'Description minimum length is 10 characters',
+          },
+          maxLength: {
+            value: 500,
+            message: 'Description maximum length is 500 characters',
+          },
+        })}
+        aria-label="description-input"
+      />
+      {errors.description && errors.description.message}
       <input type="submit" value="Submit" />
     </form>
   )
