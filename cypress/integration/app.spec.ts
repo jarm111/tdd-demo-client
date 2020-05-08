258f9305e434e08552980e47fd5509b0e8b10da8
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-05-08 13:14:52 +0300

    Add integration test for creating new event

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,3 +1,18 @@
-specify('successfully loads', () => {
+import events from '../../src/mocks/eventsMockData'
+
+it('fills new event form and adds created event to events list', () => {
+  const [{title, date, category, description}] = events
+
   cy.visit('/')
+  cy.findByLabelText('title-input')
+    .type(title)
+  cy.findByLabelText('date-input')
+    .type(date)
+  cy.findByLabelText('category-select')
+    .select(category)
+  cy.findByLabelText('description-input')
+    .type(description)
+  cy.findByText('Submit')
+    .click()
+  cy.findByText(title)
 })
