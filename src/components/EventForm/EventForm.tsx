e423c13352047c9a40358e6ac7333a934a1b4113
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-07-08 16:59:25 +0300

    Add styles to EventForm

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -32,45 +32,63 @@ const EventForm = ({ onSubmit, event }: Props) => {
 
   return (
     <form onSubmit={handleSubmit((data) => onSubmit(data as NewEvent))}>
-      <label>Title</label>
-      <input
-        name="title"
-        ref={register}
-        aria-label="title-input"
-        defaultValue={getDefaultValue('title')}
-      />
-      {errors.title && errors.title.message}
-      <label>Date</label>
-      <input
-        name="date"
-        ref={register}
-        aria-label="date-input"
-        defaultValue={getDefaultValue('date')}
-      />
-      {errors.date && errors.date.message}
-      <label>Category</label>
-      <select
-        name="category"
-        ref={register}
-        aria-label="category-select"
-        defaultValue={getDefaultValue('category')}
-      >
-        <option value="">--Select--</option>
-        {ALL_CATEGORIES.map((category) => (
-          <option key={category} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      {errors.category && errors.category.message}
-      <label>Description</label>
-      <input
-        name="description"
-        ref={register}
-        aria-label="description-input"
-        defaultValue={getDefaultValue('description')}
-      />
-      {errors.description && errors.description.message}
+      <div className="form-group">
+        <label htmlFor="title-input">Title</label>
+        <input
+          name="title"
+          ref={register}
+          id="title-input"
+          aria-label="title-input"
+          defaultValue={getDefaultValue('title')}
+        />
+        <div className="text-danger">
+          {errors.title && errors.title.message}
+        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="date-input">Date</label>
+        <input
+          name="date"
+          ref={register}
+          id="date-input"
+          aria-label="date-input"
+          defaultValue={getDefaultValue('date')}
+        />
+        <div className="text-danger">{errors.date && errors.date.message}</div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="category-select">Category</label>
+        <select
+          name="category"
+          ref={register}
+          id="category-select"
+          aria-label="category-select"
+          defaultValue={getDefaultValue('category')}
+        >
+          <option value="">--Select--</option>
+          {ALL_CATEGORIES.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        <div className="text-danger">
+          {errors.category && errors.category.message}
+        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="description-input">Description</label>
+        <textarea
+          name="description"
+          ref={register}
+          id="description-input"
+          aria-label="description-input"
+          defaultValue={getDefaultValue('description')}
+        />
+        <div className="text-danger">
+          {errors.description && errors.description.message}
+        </div>
+      </div>
       <input type="submit" value="Submit" />
     </form>
   )
